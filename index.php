9c604abc44ac5b1d30251a9ae1b5e1a217670726
akherr <akherr@vt.edu>
2016-07-05 11:35:05 -0400

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Info Grab Results</title>
+    </head>
+    <body>
+        <?php
+        $url = 'http://www.usinflationcalculator.com/inflation/';
+        $curl = curl_init($url);  
+        curl_setopt($ch, CURLOPT_HEADER, 0);  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+        $content = curl_exec($ch);
+        $source = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        echo $content;
+        // put your code here
+        ?>
+    </body>
+</html>
