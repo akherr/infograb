95c55433a906545a82030bf9b925c4dc5abb6dce
akherr <akherr@vt.edu>
2016-07-06 01:35:55 -0400

    some 4matt6

--- index.php
+++ index.php
@@ -67,9 +67,9 @@ and open the template in the editor.
         //}
         foreach($table->getElementsByTagName('tr') as $tr)
         {
-            echo '<p>' . strcmp($tr->getElementsByTagName('th')->item(0)->nodeValue, "&nbsp;") . '</p>';
+            echo '<p>' . strcmp($tr->getElementsByTagName('tds')->item(12)->nodeValue, "&nbsp;") . '</p>';
             $tds = $tr->getElementsByTagName('td'); // get the columns in this row
-            if(($tds->length == 13) and (strcmp($tr->getElementsByTagName('th')->item(0)->nodeValue, "&nbsp") !==0))
+            if(($tds->length == 13) and (strcmp($tr->getElementsByTagName('tds')->item(12)->nodeValue, "&nbsp") !==0))
             {
                 //echo $tds->item(0)->nodeValue;
                 echo '<p>Year?: ' . $tr->getElementsByTagName('th')->item(0)->nodeValue . '</p>';
