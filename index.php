68eab8af391f31ce0f1bc593bd11f8eb27fb8925
akherr <akherr@vt.edu>
2016-07-06 09:44:50 -0400

    cleanup

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 
 <?php
+//Austin Herr - July 2016 - DC Energy Code Screening
+
 $url = 'http://www.usinflationcalculator.com/inflation/';
-$curl = curl_init();  
-curl_setopt($curl, CURLOPT_URL, $url);  
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);  
+$curl = curl_init();
+
+//set option for curl to be a URL
+curl_setopt($curl, CURLOPT_URL, $url); 
+//set the curl to return as string
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+//execute and store in $content
 $content = curl_exec($curl);
-$source = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+//close the curl
 curl_close($curl);
-libxml_use_internal_errors(true);
+//libxml_use_internal_errors(true);
 $dom = new DOMDocument();
 $dom->loadHTML($content);
 $anchors = $dom->getElementsByTagName('a');
@@ -18,9 +24,7 @@ foreach ($anchors as $anchor)
 {
     if(strcmp($anchor->getAttribute('title'), "Historical Inflation Rates: 1914-2008") == 0)
     {
-        //echo '<p>FOUND HREF<p>';
         $pageurl = $anchor->getAttribute('href');
-        //echo $anchor->nodeValue, PHP_EOL;
     }
 }
 $found = false;
@@ -46,13 +50,7 @@ libxml_use_internal_errors(true);
 $dom = new DOMDocument();
 $dom->loadHTML($content);
 $table = $dom->getElementsByTagName('table')->item(0);
-//$rows = $tables->item(0)->getElementsByTagName('tr');
 
-//foreach ($rows as $row) 
-//{
-    //$cols = $row->getElementsByTagName('td');
-    //echo $cols[2];
-//}
 $minyear = 0;
 $maxyear = 0;
 $yearAvgArray = [];
@@ -61,13 +59,12 @@ $yearAvgArray = [];
 foreach($table->getElementsByTagName('tr') as $tr)
 {
 
-    $tds = $tr->getElementsByTagName('td'); // get the columns in this row
+    // get the columns in this row
+    $tds = $tr->getElementsByTagName('td'); 
     if(($tds->length == 13) and (is_numeric($tds->item(12)->nodeValue)))
     {
-        //echo $tds->item(0)->nodeValue;
         //get the current year from the th tag for the row
         $currentyear = $tr->getElementsByTagName('th')->item(0)->nodeValue;
-        //echo '<p>Year?: ' . $tr->getElementsByTagName('th')->item(0)->nodeValue . '</p>';
         if(($minyear > $currentyear) or ($minyear == 0))
         {
             $minyear = $currentyear;
@@ -77,23 +74,10 @@ foreach($table->getElementsByTagName('tr') as $tr)
             $maxyear = $currentyear;
         }
         $yearAvgArray[$currentyear] = $tds->item(12)->nodeValue;
-        //echo '<div>' . $tds->item(12)->nodeValue . '</div>';
-        //echo PHP_EOL;
-        //// check if B and D are found in column 2 and 4
-        //if(trim($tds->item(1)->nodeValue) == 'B' && trim($tds->item(3)->nodeValue) == 'D')
-        //{
-            // found B and D in the second and fourth columns
-            // echo out each column value
-            //echo $tds->item(0)->nodeValue; // A
-            //echo $tds->item(1)->nodeValue; // B
-            //echo $tds->item(2)->nodeValue; // C
-            //echo $tds->item(3)->nodeValue; // D
-            //break; // don't check any further rows
-        //}
     }
 }
 echo $description, PHP_EOL;
-echo 'This page contains data from JAN ' . $minyear . ' to DEC ' . $maxyear;
+echo 'This page contains data from JAN ' . $minyear . ' to DEC ' . $maxyear . PHP_EOL;
 echo '#------------------#' . PHP_EOL;
 echo '| year | inflation |' . PHP_EOL;
 echo '#------------------#' . PHP_EOL;
